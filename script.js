ff5bfd097092d94732b70dfaa8110022541c04f9
Miftahul Khairi <70890450+goodkey24@users.noreply.github.com>
2024-12-16 14:50:03 +0700

    Create script.js

---
+++ script.js
@@ -0,0 +1,9 @@
+let tombol = document.querySelector("button");
+
+tombol.className = "tombol";
+
+
+let ul = document.querySelector('ul');
+let li = document.createElement('li');
+li.textContent = "Item Baru";
+ul.appendChild(li);
